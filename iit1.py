8bea07895302f3f648af65d7c7bb642f3bc943da
AKHIL SINGH <79986837+akhilsingh2003@users.noreply.github.com>
2021-04-30 21:37:10 +0530

    Delete iit1.py

--- iit1.py
+++
@@ -1,8 +0,0 @@
-''' Given an integer input N (with k digits), print the sum of the each of the k digits.
-only N is given as input k is inferred from the input. For example,if the input is 153, then the output should be 35=(1**2+5**2+3**2)'''
-n=int(input())
-summ=0
-while n>0:
-    summ=summ+(n%10)*(n%10)
-    n=n//10
-print(summ)

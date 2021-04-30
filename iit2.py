cfd25acbb126ea104036185132cbd390c6d470d7
AKHIL SINGH <79986837+akhilsingh2003@users.noreply.github.com>
2021-04-30 21:37:27 +0530

    Delete iit2.py

--- iit2.py
+++
@@ -1,3 +0,0 @@
-'''Given an  m x n array A, return the index if the A[x][y]=1 for some x,y such that x is odd or y is odd ,otherwise return "No".
-For example,if we have the following array as input,then the output should be [1][2],because A[1][2]=1. 
-[0,0,0,0,0],[0,0,1,0,0]'''
